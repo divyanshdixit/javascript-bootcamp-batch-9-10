4ce33a7e1c1e963f39b5b970c6e778a0684dec78
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-07-30 22:42:54 +0530

    commit

---
+++ data-type.js
@@ -0,0 +1,40 @@
+// JS has dynamic types:
+
+/*
+
+Primitive Data types:
+var x = "divyansh" // string literal (used= this)
+
+var x = new String("divyansh")  // string object
+
+var x = null; 
+String => new String()
+number => 10, 10.01
+boolean
+undefined => typeof undefined
+
+null === undefined => false
+null == undefined => true 
+
+Complex data type:
+
+null => typeof null => Object 
+object
+arrays => Arrays are special kind of object => typeof => object
+iterating object
+
+
+var arr = [];
+// if(arr.instanceof "Array" )
+*/
+
+// Diff b/w undefiend and null:
+
+// Null (nothing)
+// null === undefined (false)
+
+// null == undefined (true)
+
+var x = ""; // empty value , 
+
+// var x = undefined/nulll (Any value can be emptied by these two)
---
+++ js-operator.js
@@ -0,0 +1,51 @@
+// airthmatic opertor (+, -, *, /, **, %)
+
+// assignement ( = )
+var f = 10
+
+// string operator (+) => to concatenate two stirng 
+
+"Divyansh" + "Dixit" 
+
+25 + "Divyanhs" => 25Divyansh
+
+
+
+25 + 25 + "Divyansh" + 25 + 25 => 50Divyansh2525
+
+// comparison opertor => (==, ===, !==, !=) => 
+
+"Divyansh" == "Divyansh" => true
+
+25 == "25" => true
+25 === "25" (value , data type ) => false
+
+// ternary operator => ? :
+() ? (  () ? '' : '' ) : ''
+
+// logical operator (&&, ||, !)
+
+("Divyansh" == "Divyansh" && 25 == "25") => true && true => true
+("Divyansh" == "Divyansh" || 25 == "25") => true || true => true
+("Divyansh" == "Divyansh" || 25 == "25") => true || false => true
+("Divyansh" === "Divyansh" || 25 === "25") => false || false => false
+
+
+// typeof operator, instanceof operator
+var x = "Divyansh";
+typeof x; // String, Number, Object
+
+
+x.instanceof "Array" // true , false
+
+// js is case senstive 
+var name
+var Name
+
+n, _, $
+
+// number as a first letter can not be used for variable name 
+
+var $
+
+
