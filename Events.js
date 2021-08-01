a66a2644fef83748cf5029cd83daaef9b8339ec6
divyanshdixitmds <divyansh@metadesignsolutions.co.uk>
2021-08-01 21:10:37 +0530

    events

--- Events.js
+++ Events.js
@@ -1,2 +1,27 @@
 // event, eventhandler(function)
 
+// events that happedn to html elements
+// js react to these events
+
+// html load finish 
+// onload 
+// onchange
+// onblur
+// onfocus
+// onkeydown
+// onclick
+// ondblclick
+
+// html allows evernt handler attributed with js code (directly added to html element)
+
+{/* <p style=""></p> */}
+
+ <p onclick="myfunction()" id="para"></p>
+
+document.getElementById('para').onclick = myfunc()
+
+// var myfunction = 
+
+document.getElementById('para').addEventListener('click', function(){
+    console.log('object')
+})
